fdabd5f465989302a2906b152e5471e59bb971f7
cazuzaneves <cazuzaneves@gmail.com>
2020-06-03 11:49:41 -0300

    unform

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -4,15 +4,20 @@ import { Container, Content, Bakcground } from "./styles";
 import { FiArrowLeft, FiMail, FiLock, FiUser } from "react-icons/fi";
 import Input from "../../components/Input";
 import Button from "../../components/Button";
+import { Form } from "@unform/web";
 
 export default () => {
+  function handleSubmit(data: any) {
+    console.log(data);
+  }
+
   return (
     <Container>
       <Bakcground />
       <Content>
         <img src={logoImg} alt="GoBarber" />
 
-        <form>
+        <Form onSubmit={handleSubmit}>
           <h1>Faça seu Cadastro</h1>
           <Input icon={FiUser} name="name" placeholder="Name" />
           <Input icon={FiMail} name="email" placeholder="E-mail" />
@@ -23,7 +28,8 @@ export default () => {
             placeholder="Senha"
           />
           <Button type="submit">Cadastrar</Button>
-        </form>
+        </Form>
+
         <a href="">
           <FiArrowLeft />
           Voltar para logon
