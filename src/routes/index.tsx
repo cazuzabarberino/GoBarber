cd3346e20271dd339c470bc8c3ec39d46889bb38
cazuzaneves <cazuzaneves@gmail.com>
2020-06-05 11:04:55 -0300

    auth in routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,17 @@
 import React from "react";
 
-import { Switch, Route } from "react-router-dom";
+import { Switch } from "react-router-dom";
 import SignIn from "../pages/SignIn";
 import SignUp from "../pages/SignUp";
+import Dahsboard from "../pages/Dashboard";
+import Route from "./Route";
 
 export default () => {
   return (
     <Switch>
       <Route path="/" exact component={SignIn} />
       <Route path="/signup" component={SignUp} />
+      <Route path="/dashboard" component={Dahsboard} isPrivate />
     </Switch>
   );
 };
